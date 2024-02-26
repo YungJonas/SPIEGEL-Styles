d40fc23dbf58d19dfc6a95112d0e9ce582aa1861
YungJonas <jonas.zwierzynski@spiegel.de>
2024-02-26 20:09:29 +0100

    Clean up code. Write 11F Textstyle into f_text {}. Change Freight Black to Freight Bold

--- src/code.ts
+++ src/code.ts
@@ -5,103 +5,10 @@
 
 //Import Color & Text Styles
 import { s_color, s_text } from "./spiegelstyles";
-import { f_color } from "./freundestyles";
+import { f_color, f_text } from "./freundestyles";
 import { m_color, m_text } from "./managerstyles";
 
 figma.showUI(__html__, { width: 176, height: 80, themeColors: true, });
-
-
-//11F TEXT STYLING//:
-
-//Headlines / 11F Galano Bold
-const HL44_Galano_Bold_tight = "S:f9be67dd23d01f57910c107ec8fa7b8aa8c4bf1f,873:0";
-const HL36_Galano_Bold_tight = "S:29568856de4320f7a3fde9b7d92fd266bf94b6c8,873:2";
-const HL32_Galano_Bold_tight = "S:6d70773dcd67b3df02a9758bbae1ee7e9ea30581,873:3";
-const HL28_Galano_Bold_tight = "S:26e949321fdab0ecc228bcb02f32e82765d3d8ec,873:4";
-const HL24_Galano_Bold_tight = "S:af9206ff23ee5c1cb85ce10c93cb15ae28fcd05f,873:5";
-const HL206_Galano_Bold_tight = "S:4c4a035d595bf09cea2a51b12349223cbcfd3dd1,873:6";
-const HL18_Galano_Bold_tight = "S:bfe5daa213eaa6aba630958fdc6221b6a028e850,873:7";
-const HL168_Galano_Bold_tight = "S:2a0c4cbe143c21e43408f97ffdb0d384b5385984,873:9";
-
-//Headlines / Freight Text Pro Black / Opinion
-const HL40_Freight_Black_Italic = "S:744467c583e91ae651e697b3c81ff8bff4b87c58,1105:6";
-const HL36_Freight_Black_Italic = "S:004a5dde572924c0ba4a5649701b724f53deec3d,1105:9";
-const HL32_Freight_Black_Italic = "S:e8cec257851ac230fef799dc472a17bd32178065,1105:12";
-const HL28_Freight_Black_Italic = "S:6b34afd6188c108f13ed898d10b3ad61563a7941,1105:15";
-const HL24_Freight_Black_Italic = "S:afef54e8cd6d9236f9204f65c627c69c1de78082,1105:24";
-const HL206_Freight_Black_Italic = "S:2026d833fac47c93f4f824e28403160b5c5f39d7,1105:18";
-const HL18_Freight_Black_Italic = "S:5dd294240c6c0087458b241307bbab4ab2d438f8,1105:21";
-const HL168_Freight_Black_Italic = "S:c22e6d5009af6a9130d5f7cbf872a3a77d528699,1105:27";
-const HL14_Freight_Black_Italic = "S:9cf0684bbff8d0fdd2808315d99712d175c15566,1105:30";
-
-//Headlines / Freight Text Pro Back / Highlight
-const HL40_Freight_Book = "S:6c372cdd7be20c94a5b397132c21c8cc6f12aa4e,1093:14";
-const HL32_Freight_Book = "S:fef9d9f189cf10465a2e65f6e4a468e22321545b,1093:18";
-const HL28_Freight_Book = "S:af17df413a6a61ad5eba67a803df18cf1eb1a655,1093:22";
-const HL24_Freight_Book = "S:b302218b223d56b9444ec347a81b3b33336dd1ed,1093:27";
-const HL18_Freight_Book = "S:51fdbf5606e42974f1c0d6726d5dac07252f3f94,1093:31";
-
-//Headlines / 11F Galano
-const HL28_Galano_Bold = "S:3a06ebbbcaff6560dc8c429b24ea30c45455b2fc,475:81";
-const HL24_Galano_Bold = "S:8fba8fae8e9f89bb0cb22e6d9181dcd86a8c1b39,475:82";
-const HL206_Galano_Bold = "S:ae97b6bb3b0d67ae0272ddeda354c45f7c8aba62,475:145";
-const HL18_Galano_Bold = "S:c6bea5d1f374f77dcb0b2076bc882d1e36dc50f4,475:84";
-const HL18_Galano_Regular = "S:207086c31118aa31014e046172fd84f63288c577,475:147";
-const HL168_Galano_Bold = "S:3724fd1a99393dea507870a500dbf1565481a4cb,475:85";
-const HL168_Galano_Regular = "S:2a248e1b77075fb042887e6d33cf3fb7aad447a0,475:86";
-const HL14_Galano_Bold = "S:850d517fd149dbe9398c4efd0d594f37d73da0b7,475:87";
-const HL14_Galano_Regular = "S:710c89bd7e025c211a18351d397987a48bce51d8,475:88";
-
-//Text / 11F Galano
-const Text206_Freight_Book = "S:8042a37b95d972d4653849ad5bc350672a5d42a3,873:28";
-const Text18_Freight_Book = "S:de969486d9cfec16ea7a256406d3a481b4248171,873:29";
-const Text168_Galano_Bold_loose = "S:30f7cd849293c9c1264f35e66b4c333a61f61c45,873:15";
-const Text168_Galano_Regular_loose = "S:f8543ee33af8aff68733ba46b1e49647489f6abb,873:16";
-const Text14_Galano_Bold_loose = "S:3440b55f03cf408e2ec0239fd1aca4b05ff32262,873:17";
-const Text14_Galano_Regular_loose = "S:8a1105fb79b4b4b91c92d885c373142c74907eb6,873:18";
-
-//Text /  Freight Text Pro
-const Text206_Freight_Bold_loose = "S:6ccdcdc5246e8097165afa13c0e2c2d0b955c3d5,890:3";
-const Text206_Freight_Bold_Italic_loose = "S:aad9963da3867763b4106d3405a71df7ca9332ad,890:5";
-const Text206_Freight_Book_loose = "S:844887cfd10dc9edf35897b10e0bdf89d9285a06,890:7";
-const Text206_Freight_Book_Italic_loose = "S:e04ad103b0dbf2a4c2298e9f4183e0053ba7b460,890:9";
-const Text18_Freight_Bold_loose = "S:36eea42e4527e4ffa27fe087d8ed7209f23c0fb5,890:11";
-const Text18_Freight_Bold_Italic_loose = "S:81ec88f22f225f65fcf5025d6e6a9ded9f8c48d1,890:13";
-const Text18_Freight_Book_loose = "S:59d3275f97a65785fa56fe8d748422562849701f,890:15";
-const Text18_Freight_Book_Italic_loose = "S:f2c1709bee20af93c5d80608590e2fd19f01dec4,890:17";
-const Text168_Freight_Book_loose = "S:4cc7bb806701fc2de8d3959dc2f2918b7bb33e01,873:20";
-
-//UI /  Freight Text Pro
-const UI206_Freight_Bold = "S:93d6b0aa37c11f0344c07afebb6361c207e9998e,475:106";
-const UI206_Freight_Book = "S:f1ee2f898b27f0d8b2efc60533a5763cb80fc8cc,475:107";
-const UI18_Freight_Bold = "S:33b23e712d3400b419a54ae423166149b3cf63fb,475:108";
-const UI18_Freight_Book = "S:e28a63c9b960b4970e94b3b0a84dedb5dd5dd329,475:109";
-
-//UI / 11F Galano
-const UI168_Galano_Bold = "S:7a107af1794d0e2aa8a161a6cc1f9540ffc7287c,475:110";
-const UI168_Galano_Regular = "S:00f121252b25f26c29fdae414492041d68e6ffc1,475:111";
-const UI14_Galano_Bold = "S:dd9ad8fe8d745dca4e58160ff89c3cabb3dfe1de,475:112";
-const UI14_Galano_Regular = "S:c2ac76b697cfed8acbdad03dbd6229d0db94ef29,475:113";
-const UI14_Galano_Regular_caps_xwide = "S:b987a92656503d4166e490e89da2a784b7ab1e3c,475:114";
-const UI12_Galano_Regular = "S:f77ee900b4f7a2faf0d88ed88c6d2b649ab5cf66,475:122";
-const UI12_Galano_Regular_wide = "S:6c4a9c95653355692e0791ef48a20c28008704b9,475:116";
-const UI12_Galano_Bold_caps_xwide = "S:9dfc564209d0482d20b2fa25de1aab49eeb9009b,475:117";
-const UI12_Galano_Regular_caps_xwide = "S:66b6e2a96b182970894ed12ce7228ffaf92be926,475:118";
-const UI10_Galano_Regular = "S:656b13d3715c412c5a528b3776080004929ad02a,475:123";
-
-//Special /  Freight Text Pro Book
-const Special64_Freight_Book = "S:9d93966cf03b09d49c91490e2d47c82da3e7afd1,137:57";
-const Special58_Freight_Book = "S:4c44e97e2aa37b46d5c7e711e5c973131ed8245a,137:61";
-const Special28_Freight_Book = "S:60e71d87c0b60671f4145683125da6951df62d6b,137:65";
-const Special24_Freight_Book = "S:3a7171b73d3fbcfbc0f6694361a644a99b24ce9a,137:69";
-
-//Special / Freight Text Pro Black
-const Special28_Freight_Black_Italic = "S:d7c9861ef5f69a45880411cd4ffee0e216b9c62a,1105:35";
-const Special24_Freight_Black_Italic = "S:41bc67be7a986864181621e55d859ade1be51452,1105:45";
-
-//Special / 11F Galano Bold
-const Special28_Galano_Bold = "S:796d1a4ec121d17f78fd715bdfb3b3052fd9d2f5,475:120";
-const Special24_Galano_Bold = "S:9f894acb1ce6f99bdfca50fb3a474df78ad20051,475:121";
 
 
 // SPIEGEL TO 11Freunde Mappings //
@@ -124,6 +31,8 @@ const f_ColorMap = {
   [s_color.secondary2_darker]: f_color.secondary2_darker,
   [s_color.secondary2_dark]: f_color.secondary2_dark,
   [s_color.secondary2_lightest]: f_color.secondary2_lightest,
+  [s_color.dm_primary_base]: f_color.dm_primary_base,
+  [s_color.dm_shade_darkest]: f_color.dm_shade_darkest,
 };
 
 const m_ColorMap = {
@@ -151,78 +60,78 @@ const m_ColorMap = {
 const f_textMap = {
   // HEADLINES Mapping
   //[HL48_Slab_Cd_Extrabold_tight]: "No direct match in 11Freunde",
-  [s_text.HL44_Slab_Cd_Extrabold_tight]: HL44_Galano_Bold_tight,
+  [s_text.HL44_Slab_Cd_Extrabold_tight]: f_text.HL44_Galano_Bold_tight,
   //[HL40_Slab_Cd_Extrabold_tight]: "No direct match in 11Freunde",
-  [s_text.HL36_Slab_Cd_Extrabold_tight]: HL36_Galano_Bold_tight,
-  [s_text.HL32_Slab_Cd_Extrabold_tight]: HL32_Galano_Bold_tight,
-  [s_text.HL28_Slab_Cd_Extrabold_tight]: HL28_Galano_Bold_tight,
-  [s_text.HL24_Slab_Cd_Extrabold_tight]: HL24_Galano_Bold_tight,
-  [s_text.HL20_Slab_Cd_Extrabold_tight]: HL206_Galano_Bold_tight,
-  [s_text.HL18_Slab_Cd_Extrabold_tight]: HL18_Galano_Bold_tight,
-  [s_text.HL16_Slab_Cd_Extrabold_tight]: HL168_Galano_Bold_tight,
+  [s_text.HL36_Slab_Cd_Extrabold_tight]: f_text.HL36_Galano_Bold_tight,
+  [s_text.HL32_Slab_Cd_Extrabold_tight]: f_text.HL32_Galano_Bold_tight,
+  [s_text.HL28_Slab_Cd_Extrabold_tight]: f_text.HL28_Galano_Bold_tight,
+  [s_text.HL24_Slab_Cd_Extrabold_tight]: f_text.HL24_Galano_Bold_tight,
+  [s_text.HL20_Slab_Cd_Extrabold_tight]: f_text.HL206_Galano_Bold_tight,
+  [s_text.HL18_Slab_Cd_Extrabold_tight]: f_text.HL18_Galano_Bold_tight,
+  [s_text.HL16_Slab_Cd_Extrabold_tight]: f_text.HL168_Galano_Bold_tight,
 
-  [s_text.HL40_Serif_Display_Bold_Italic]: HL40_Freight_Black_Italic,
-  [s_text.HL36_Serif_Display_Bold_Italic]: HL36_Freight_Black_Italic,
-  [s_text.HL32_Serif_Display_Bold_Italic]: HL32_Freight_Black_Italic,
-  [s_text.HL28_Serif_Display_Bold_Italic]: HL28_Freight_Black_Italic,
-  [s_text.HL24_Serif_Display_Bold_Italic]: HL24_Freight_Black_Italic,
-  [s_text.HL20_Serif_Display_Bold_Italic]: HL206_Freight_Black_Italic,
-  [s_text.HL18_Serif_Display_Bold_Italic]: HL18_Freight_Black_Italic,
-  [s_text.HL16_Serif_Display_Bold_Italic]: HL168_Freight_Black_Italic,
-  [s_text.HL14_Serif_Display_Bold_Italic]: HL14_Freight_Black_Italic,
+  [s_text.HL40_Serif_Display_Bold_Italic]: f_text.HL40_Freight_Bold_Italic,
+  [s_text.HL36_Serif_Display_Bold_Italic]: f_text.HL36_Freight_Bold_Italic,
+  [s_text.HL32_Serif_Display_Bold_Italic]: f_text.HL32_Freight_Bold_Italic,
+  [s_text.HL28_Serif_Display_Bold_Italic]: f_text.HL28_Freight_Bold_Italic,
+  [s_text.HL24_Serif_Display_Bold_Italic]: f_text.HL24_Freight_Bold_Italic,
+  [s_text.HL20_Serif_Display_Bold_Italic]: f_text.HL206_Freight_Bold_Italic,
+  [s_text.HL18_Serif_Display_Bold_Italic]: f_text.HL18_Freight_Bold_Italic,
+  [s_text.HL16_Serif_Display_Bold_Italic]: f_text.HL168_Freight_Bold_Italic,
+  [s_text.HL14_Serif_Display_Bold_Italic]: f_text.HL14_Freight_Bold_Italic,
 
-  [s_text.HL40_Serif_Text_Regular]: HL40_Freight_Book,
-  [s_text.HL32_Serif_Text_Regular]: HL32_Freight_Book,
-  [s_text.HL28_Serif_Text_Regular]: HL28_Freight_Book,
-  [s_text.HL24_Serif_Text_Regular]: HL24_Freight_Book,
-  [s_text.HL18_Serif_Text_Regular]: HL18_Freight_Book,
+  [s_text.HL40_Serif_Text_Regular]: f_text.HL40_Freight_Book,
+  [s_text.HL32_Serif_Text_Regular]: f_text.HL32_Freight_Book,
+  [s_text.HL28_Serif_Text_Regular]: f_text.HL28_Freight_Book,
+  [s_text.HL24_Serif_Text_Regular]: f_text.HL24_Freight_Book,
+  [s_text.HL18_Serif_Text_Regular]: f_text.HL18_Freight_Book,
 
-  [s_text.HL28_Sans_Bold]: HL28_Galano_Bold,
-  [s_text.HL24_Sans_Bold]: HL24_Galano_Bold,
-  [s_text.HL20_Sans_Bold]: HL24_Galano_Bold,
-  [s_text.HL18_Sans_Bold]: HL18_Galano_Bold,
-  [s_text.HL18_Sans_Regular]: HL18_Galano_Regular,
-  [s_text.HL16_Sans_Bold]: HL168_Galano_Bold,
-  [s_text.HL16_Sans_Regular]: HL168_Galano_Regular,
-  [s_text.HL14_Sans_Bold]: HL14_Galano_Bold,
-  [s_text.HL14_Sans_Regular]: HL14_Galano_Regular,
+  [s_text.HL28_Sans_Bold]: f_text.HL28_Galano_Bold,
+  [s_text.HL24_Sans_Bold]: f_text.HL24_Galano_Bold,
+  [s_text.HL20_Sans_Bold]: f_text.HL24_Galano_Bold,
+  [s_text.HL18_Sans_Bold]: f_text.HL18_Galano_Bold,
+  [s_text.HL18_Sans_Regular]: f_text.HL18_Galano_Regular,
+  [s_text.HL16_Sans_Bold]: f_text.HL168_Galano_Bold,
+  [s_text.HL16_Sans_Regular]: f_text.HL168_Galano_Regular,
+  [s_text.HL14_Sans_Bold]: f_text.HL14_Galano_Bold,
+  [s_text.HL14_Sans_Regular]: f_text.HL14_Galano_Regular,
 
   // TEXT Mapping
-  [s_text.Text20_Sans_Regular_loose]: Text206_Freight_Book, //Sonderlösung 11F
-  [s_text.Text18_Sans_Regular_loose]: Text18_Freight_Book,  //Sonderlösung 11F
-  [s_text.Text16_Sans_Bold_loose]: Text168_Galano_Bold_loose,
-  [s_text.Text16_Sans_Regular_loose]: Text168_Galano_Regular_loose,
-  [s_text.Text14_Sans_Bold_loose]: Text14_Galano_Bold_loose,
-  [s_text.Text14_Sans_Regular_loose]: Text14_Galano_Regular_loose,
+  [s_text.Text20_Sans_Regular_loose]: f_text.Text206_Freight_Book, //Sonderlösung 11F
+  [s_text.Text18_Sans_Regular_loose]: f_text.Text18_Freight_Book,  //Sonderlösung 11F
+  [s_text.Text16_Sans_Bold_loose]: f_text.Text168_Galano_Bold_loose,
+  [s_text.Text16_Sans_Regular_loose]: f_text.Text168_Galano_Regular_loose,
+  [s_text.Text14_Sans_Bold_loose]: f_text.Text14_Galano_Bold_loose,
+  [s_text.Text14_Sans_Regular_loose]: f_text.Text14_Galano_Regular_loose,
 
-  [s_text.Text20_Serif_Text_Bold_xloose]: Text206_Freight_Bold_loose, //Sonderlösung 11F
-  [s_text.Text20_Serif_Text_Bold_Italic_xloose]: Text206_Freight_Bold_Italic_loose, //Sonderlösung 11F
-  [s_text.Text20_Serif_Text_Regular_xloose]: Text206_Freight_Book_loose, //Sonderlösung 11F
-  [s_text.Text20_Serif_Text_Italic_xloose]: Text206_Freight_Book_Italic_loose, //Sonderlösung 11F
-  [s_text.Text18_Serif_Text_Bold_xloose]: Text168_Freight_Book_loose, //Sonderlösung 11F
-  [s_text.Text18_Serif_Text_Bold_Italic_xloose]: Text18_Freight_Book_Italic_loose, //Sonderlösung 11F
-  [s_text.Text18_Serif_Text_Regular_xloose]: Text18_Freight_Book_loose, //Sonderlösung 11F
-  [s_text.Text18_Serif_Text_Italic_xloose]: Text18_Freight_Book_Italic_loose, //Sonderlösung 11F
-  [s_text.Text18_Serif_Text_Regular_loose]: Text18_Freight_Book_loose,
-  [s_text.Text16_Serif_Text_Regular_loose]: Text168_Freight_Book_loose,
+  [s_text.Text20_Serif_Text_Bold_xloose]: f_text.Text206_Freight_Bold_loose, //Sonderlösung 11F
+  [s_text.Text20_Serif_Text_Bold_Italic_xloose]: f_text.Text206_Freight_Bold_Italic_loose, //Sonderlösung 11F
+  [s_text.Text20_Serif_Text_Regular_xloose]: f_text.Text206_Freight_Book_loose, //Sonderlösung 11F
+  [s_text.Text20_Serif_Text_Italic_xloose]: f_text.Text206_Freight_Book_Italic_loose, //Sonderlösung 11F
+  [s_text.Text18_Serif_Text_Bold_xloose]: f_text.Text18_Freight_Bold_loose, //Sonderlösung 11F
+  [s_text.Text18_Serif_Text_Bold_Italic_xloose]: f_text.Text18_Freight_Bold_Italic_loose, //Sonderlösung 11F
+  [s_text.Text18_Serif_Text_Regular_xloose]: f_text.Text18_Freight_Book_loose, //Sonderlösung 11F
+  [s_text.Text18_Serif_Text_Italic_xloose]: f_text.Text18_Freight_Book_Italic_loose, //Sonderlösung 11F
+  [s_text.Text18_Serif_Text_Regular_loose]: f_text.Text18_Freight_Book_loose,
+  [s_text.Text16_Serif_Text_Regular_loose]: f_text.Text168_Freight_Book_loose,
 
   // UI Mapping
-  [s_text.UI20_Serif_Text_Bold_Italic]: UI206_Freight_Bold,
-  [s_text.UI20_Serif_Text_Italic]: UI206_Freight_Book,
-  [s_text.UI18_Serif_Text_Bold_Italic]: UI18_Freight_Bold,
-  [s_text.UI18_Serif_Text_Italic]: UI18_Freight_Book,
+  [s_text.UI20_Serif_Text_Bold_Italic]: f_text.UI206_Freight_Bold,
+  [s_text.UI20_Serif_Text_Italic]: f_text.UI206_Freight_Book,
+  [s_text.UI18_Serif_Text_Bold_Italic]: f_text.UI18_Freight_Bold,
+  [s_text.UI18_Serif_Text_Italic]: f_text.UI18_Freight_Book,
 
-  [s_text.UI16_Sans_Bold]: UI168_Galano_Bold,
-  [s_text.UI16_Sans_Regular]: UI168_Galano_Regular,
-  [s_text.UI14_Sans_Bold]: UI14_Galano_Bold,
-  [s_text.UI14_Sans_Regular]: UI14_Galano_Regular,
-  [s_text.UI14_Sans_Regular_caps_xwide]: UI14_Galano_Regular_caps_xwide,
+  [s_text.UI16_Sans_Bold]: f_text.UI168_Galano_Bold,
+  [s_text.UI16_Sans_Regular]: f_text.UI168_Galano_Regular,
+  [s_text.UI14_Sans_Bold]: f_text.UI14_Galano_Bold,
+  [s_text.UI14_Sans_Regular]: f_text.UI14_Galano_Regular,
+  [s_text.UI14_Sans_Regular_caps_xwide]: f_text.UI14_Galano_Regular_caps_xwide,
   //[UI14_Sans_Bold_caps_xwide]: UI14_Galano_Bold_xwide, //MISSING STYLE?
-  [s_text.UI12_Sans_Regular]: UI12_Galano_Regular,
-  [s_text.UI12_Sans_Regular_wide]: UI12_Galano_Regular_wide,
-  [s_text.UI12_Sans_Bold_caps_xwide]: UI12_Galano_Bold_caps_xwide,
-  [s_text.UI12_Sans_Regular_caps_xwide]: UI12_Galano_Regular_caps_xwide,
-  [s_text.UI10_Sans_Regular]: UI10_Galano_Regular,
+  [s_text.UI12_Sans_Regular]: f_text.UI12_Galano_Regular,
+  [s_text.UI12_Sans_Regular_wide]: f_text.UI12_Galano_Regular_wide,
+  [s_text.UI12_Sans_Bold_caps_xwide]: f_text.UI12_Galano_Bold_caps_xwide,
+  [s_text.UI12_Sans_Regular_caps_xwide]: f_text.UI12_Galano_Regular_caps_xwide,
+  [s_text.UI10_Sans_Regular]: f_text.UI10_Galano_Regular,
 }
 
 // Counting fillStyles and textStyles in current selection and returning the values.
@@ -297,7 +206,7 @@ figma.ui.onmessage = msg => {
   fillStyleChanges = 0;
   textStyleChanges = 0;
 
-};
+}
 
 
 
@@ -310,6 +219,15 @@ function changeColor(node: SceneNode, f_ColorMap: { [key: string]: string }): vo
   if ('fillStyleId' in node && node.fillStyleId in f_ColorMap) {
     // Replace the current fillStyleId with the new one from the mapping
     node.fillStyleId = f_ColorMap[node.fillStyleId as string];
+    fillStyleChanges++; // Increment counter
+  }
+  // If the node has children, apply the function recursively to each child
+  if ('children' in node) {
+    node.children.forEach(child => changeColor(child, f_ColorMap));
+  }
+  if ('strokeStyleId' in node && node.strokeStyleId in f_ColorMap) {
+    // Replace the current strokeStyleId with the new one from the mapping
+    node.strokeStyleId = f_ColorMap[node.strokeStyleId as string];
     fillStyleChanges++; // Increment counter
   }
   // If the node has children, apply the function recursively to each child
--- src/freundestyles.ts
+++ src/freundestyles.ts
@@ -20,6 +20,105 @@ const f_color = {
     secondary2_darker: "S:52cb0ec93acbc8504130f4ec20d76e287c91fbfa,442:2",
     secondary2_dark: "S:51f9c35887d1a36dc8e5feb48ccda41c2cfc36b6,442:1",
     secondary2_lightest: "S:9eb47657e7747f15f478e6dfa94fd734587c5482,442:0",
+    // F-Darkmode
+    dm_shade_darkest: "S:2e616e8a158ef1196152c8afd8df3a1a6f5307d2,435:1",
+    dm_primary_base: "S:112130ee7bd931856aa13ee54ab63b58b06705a8,1622:1",
+
 };
 
-export { f_color };
+const f_text = {
+    //11F TEXT STYLING//:
+
+    //Headlines / 11F Galano Bold
+    HL44_Galano_Bold_tight: "S:f9be67dd23d01f57910c107ec8fa7b8aa8c4bf1f,873:0",
+    HL36_Galano_Bold_tight: "S:29568856de4320f7a3fde9b7d92fd266bf94b6c8,873:2",
+    HL32_Galano_Bold_tight: "S:6d70773dcd67b3df02a9758bbae1ee7e9ea30581,873:3",
+    HL28_Galano_Bold_tight: "S:26e949321fdab0ecc228bcb02f32e82765d3d8ec,873:4",
+    HL24_Galano_Bold_tight: "S:af9206ff23ee5c1cb85ce10c93cb15ae28fcd05f,873:5",
+    HL206_Galano_Bold_tight: "S:4c4a035d595bf09cea2a51b12349223cbcfd3dd1,873:6",
+    HL18_Galano_Bold_tight: "S:bfe5daa213eaa6aba630958fdc6221b6a028e850,873:7",
+    HL168_Galano_Bold_tight: "S:2a0c4cbe143c21e43408f97ffdb0d384b5385984,873:9",
+
+    //Headlines / Freight Text Pro Bold / Opinion
+    HL40_Freight_Bold_Italic: "S:744467c583e91ae651e697b3c81ff8bff4b87c58,1105:6",
+    HL36_Freight_Bold_Italic: "S:004a5dde572924c0ba4a5649701b724f53deec3d,1105:9",
+    HL32_Freight_Bold_Italic: "S:e8cec257851ac230fef799dc472a17bd32178065,1105:12",
+    HL28_Freight_Bold_Italic: "S:6b34afd6188c108f13ed898d10b3ad61563a7941,1105:15",
+    HL24_Freight_Bold_Italic: "S:afef54e8cd6d9236f9204f65c627c69c1de78082,1105:24",
+    HL206_Freight_Bold_Italic: "S:2026d833fac47c93f4f824e28403160b5c5f39d7,1105:18",
+    HL18_Freight_Bold_Italic: "S:5dd294240c6c0087458b241307bbab4ab2d438f8,1105:21",
+    HL168_Freight_Bold_Italic: "S:c22e6d5009af6a9130d5f7cbf872a3a77d528699,1105:27",
+    HL14_Freight_Bold_Italic: "S:9cf0684bbff8d0fdd2808315d99712d175c15566,1105:30",
+
+    //Headlines / Freight Text Pro Back / Highlight
+    HL40_Freight_Book: "S:6c372cdd7be20c94a5b397132c21c8cc6f12aa4e,1093:14",
+    HL32_Freight_Book: "S:fef9d9f189cf10465a2e65f6e4a468e22321545b,1093:18",
+    HL28_Freight_Book: "S:af17df413a6a61ad5eba67a803df18cf1eb1a655,1093:22",
+    HL24_Freight_Book: "S:b302218b223d56b9444ec347a81b3b33336dd1ed,1093:27",
+    HL18_Freight_Book: "S:51fdbf5606e42974f1c0d6726d5dac07252f3f94,1093:31",
+
+    //Headlines / 11F Galano
+    HL28_Galano_Bold: "S:3a06ebbbcaff6560dc8c429b24ea30c45455b2fc,475:81",
+    HL24_Galano_Bold: "S:8fba8fae8e9f89bb0cb22e6d9181dcd86a8c1b39,475:82",
+    HL206_Galano_Bold: "S:ae97b6bb3b0d67ae0272ddeda354c45f7c8aba62,475:145",
+    HL18_Galano_Bold: "S:c6bea5d1f374f77dcb0b2076bc882d1e36dc50f4,475:84",
+    HL18_Galano_Regular: "S:207086c31118aa31014e046172fd84f63288c577,475:147",
+    HL168_Galano_Bold: "S:3724fd1a99393dea507870a500dbf1565481a4cb,475:85",
+    HL168_Galano_Regular: "S:2a248e1b77075fb042887e6d33cf3fb7aad447a0,475:86",
+    HL14_Galano_Bold: "S:850d517fd149dbe9398c4efd0d594f37d73da0b7,475:87",
+    HL14_Galano_Regular: "S:710c89bd7e025c211a18351d397987a48bce51d8,475:88",
+
+    //Text / 11F Galano
+    Text206_Freight_Book: "S:8042a37b95d972d4653849ad5bc350672a5d42a3,873:28",
+    Text18_Freight_Book: "S:de969486d9cfec16ea7a256406d3a481b4248171,873:29",
+    Text168_Galano_Bold_loose: "S:30f7cd849293c9c1264f35e66b4c333a61f61c45,873:15",
+    Text168_Galano_Regular_loose: "S:f8543ee33af8aff68733ba46b1e49647489f6abb,873:16",
+    Text14_Galano_Bold_loose: "S:3440b55f03cf408e2ec0239fd1aca4b05ff32262,873:17",
+    Text14_Galano_Regular_loose: "S:8a1105fb79b4b4b91c92d885c373142c74907eb6,873:18",
+
+    //Text /  Freight Text Pro
+    Text206_Freight_Bold_loose: "S:6ccdcdc5246e8097165afa13c0e2c2d0b955c3d5,890:3",
+    Text206_Freight_Bold_Italic_loose: "S:aad9963da3867763b4106d3405a71df7ca9332ad,890:5",
+    Text206_Freight_Book_loose: "S:844887cfd10dc9edf35897b10e0bdf89d9285a06,890:7",
+    Text206_Freight_Book_Italic_loose: "S:e04ad103b0dbf2a4c2298e9f4183e0053ba7b460,890:9",
+    Text18_Freight_Bold_loose: "S:36eea42e4527e4ffa27fe087d8ed7209f23c0fb5,890:11",
+    Text18_Freight_Bold_Italic_loose: "S:81ec88f22f225f65fcf5025d6e6a9ded9f8c48d1,890:13",
+    Text18_Freight_Book_loose: "S:59d3275f97a65785fa56fe8d748422562849701f,890:15",
+    Text18_Freight_Book_Italic_loose: "S:f2c1709bee20af93c5d80608590e2fd19f01dec4,890:17",
+    Text168_Freight_Book_loose: "S:4cc7bb806701fc2de8d3959dc2f2918b7bb33e01,873:20",
+
+    //UI /  Freight Text Pro
+    UI206_Freight_Bold: "S:93d6b0aa37c11f0344c07afebb6361c207e9998e,475:106",
+    UI206_Freight_Book: "S:f1ee2f898b27f0d8b2efc60533a5763cb80fc8cc,475:107",
+    UI18_Freight_Bold: "S:33b23e712d3400b419a54ae423166149b3cf63fb,475:108",
+    UI18_Freight_Book: "S:e28a63c9b960b4970e94b3b0a84dedb5dd5dd329,475:109",
+
+    //UI / 11F Galano
+    UI168_Galano_Bold: "S:7a107af1794d0e2aa8a161a6cc1f9540ffc7287c,475:110",
+    UI168_Galano_Regular: "S:00f121252b25f26c29fdae414492041d68e6ffc1,475:111",
+    UI14_Galano_Bold: "S:dd9ad8fe8d745dca4e58160ff89c3cabb3dfe1de,475:112",
+    UI14_Galano_Regular: "S:c2ac76b697cfed8acbdad03dbd6229d0db94ef29,475:113",
+    UI14_Galano_Regular_caps_xwide: "S:b987a92656503d4166e490e89da2a784b7ab1e3c,475:114",
+    UI12_Galano_Regular: "S:f77ee900b4f7a2faf0d88ed88c6d2b649ab5cf66,475:122",
+    UI12_Galano_Regular_wide: "S:6c4a9c95653355692e0791ef48a20c28008704b9,475:116",
+    UI12_Galano_Bold_caps_xwide: "S:9dfc564209d0482d20b2fa25de1aab49eeb9009b,475:117",
+    UI12_Galano_Regular_caps_xwide: "S:66b6e2a96b182970894ed12ce7228ffaf92be926,475:118",
+    UI10_Galano_Regular: "S:656b13d3715c412c5a528b3776080004929ad02a,475:123",
+
+    //Special /  Freight Text Pro Book
+    Special64_Freight_Book: "S:9d93966cf03b09d49c91490e2d47c82da3e7afd1,137:57",
+    Special58_Freight_Book: "S:4c44e97e2aa37b46d5c7e711e5c973131ed8245a,137:61",
+    Special28_Freight_Book: "S:60e71d87c0b60671f4145683125da6951df62d6b,137:65",
+    Special24_Freight_Book: "S:3a7171b73d3fbcfbc0f6694361a644a99b24ce9a,137:69",
+
+    //Special / Freight Text Pro Bold
+    Special28_Freight_Bold_Italic: "S:d7c9861ef5f69a45880411cd4ffee0e216b9c62a,1105:35",
+    Special24_Freight_Bold_Italic: "S:41bc67be7a986864181621e55d859ade1be51452,1105:45",
+
+    //Special / 11F Galano Bold
+    Special28_Galano_Bold: "S:796d1a4ec121d17f78fd715bdfb3b3052fd9d2f5,475:120",
+    Special24_Galano_Bold: "S:9f894acb1ce6f99bdfca50fb3a474df78ad20051,475:121",
+
+}
+
+export { f_color, f_text };
